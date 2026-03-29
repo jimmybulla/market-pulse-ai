b5d7fd33f8c70ff3abfb65468811ab2cf5293e68
chidozie jim <chidoziejim@chidozies-MacBook-Pro.local>
2026-03-29 16:45:06 +0100

    feat: add /stocks page

---
+++ frontend/app/stocks/page.tsx
@@ -0,0 +1,15 @@
+import { getStocks } from '@/lib/api'
+import StocksTable from '@/components/stocks/StocksTable'
+import TopBar from '@/components/layout/TopBar'
+
+export default async function StocksPage() {
+  const { data } = await getStocks({ limit: 200 })
+  return (
+    <div>
+      <TopBar title="Stocks" subtitle="All tracked stocks" />
+      <div className="p-6">
+        <StocksTable stocks={data} />
+      </div>
+    </div>
+  )
+}
